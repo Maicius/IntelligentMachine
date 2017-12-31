4a5f71d9cde91679f24194d4758518e1fd882d42
mai ciu s <maicius@outlook.com>
2017-12-31 23:47:47 +0800

    计算协方差，剩余特征185

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# encoding = utf-8
+# coding=utf-8
 
 import pandas as pd
 import numpy as np
@@ -7,7 +7,7 @@ from sklearn import preprocessing
 
 def pre_process_data():
     print("begin to read data")
-    train_data = pd.read_excel('small.xlsx')
+    train_data = pd.read_excel('train.xlsx')
     # 去掉空行
     print("raw_data:" + str(train_data.shape))
     nan_data_value = remove_nan_data(train_data)
@@ -25,6 +25,10 @@ def pre_process_data():
     train_data.drop(['Y'], axis=1, inplace=True)
     x_train = normalize_data(train_data)
     x_train.fillna(x_train.mean())
+    corr_df = cal_corrcoef(x_train, y_train)
+    corr02 = corr_df[corr_df.corr_value >= 0.2]
+    corr02_col = corr02['col'].values.tolist()
+    x_train = x_train[corr02_col]
     print("Finish preprocess")
     print(x_train.shape, y_train.shape)
     return x_train, y_train
@@ -34,7 +38,7 @@ def remove_nan_data(data):
     nan_data = data.isnull().sum(axis=0).reset_index()
     nan_data.columns = ['col', 'nan_count']
     nan_data = nan_data.sort_values(by='nan_count')
-    nan_data_value = nan_data[nan_data.nan_count > 20].col.values
+    nan_data_value = nan_data[nan_data.nan_count > 200].col.values
     print("nan_data_value:" + str(nan_data_value))
     return nan_data_value
 
@@ -44,6 +48,18 @@ def remove_no_float(data):
     data_type = data.dtypes.reset_index()
     data_type.columns = ['col', 'dtype']
     return data_type[data_type.dtype != 'object'].col.values
+
+
+# 计算协方差
+def cal_corrcoef(float_df, y_train):
+    corr_values = []
+    float_col = list(float_df.columns)
+    for col in float_col:
+        corr_values.append(abs(np.corrcoef(float_df[col].values, y_train) \
+                                   [0, 1]))
+    corr_df = pd.DataFrame({'col': float_col, 'corr_value': corr_values})
+    corr_df = corr_df.sort_values(by='corr_value', ascending=False)
+    return corr_df
 
 
 # 去掉数字相同的列以及日期列
