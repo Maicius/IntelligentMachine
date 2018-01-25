c1fe3b4ca8f1d336161e8acca69e368693defc3d
mai ciu s <maicius@outlook.com>
2018-01-25 14:24:45 +0800

    融合

--- mse.py
+++ mse.py
@@ -46,12 +46,21 @@ third = pd.read_csv('result/submitB_A5-0.022075-0.04593.csv', header=None).reset
 fourth = pd.read_csv('result/xgboost-0.0264-0.03771.csv', header=None).reset_index().drop([0], axis=1, inplace=False)
 fifth = pd.read_csv('result/xgboost4-0.02437-0.04045.csv', header=None).reset_index().drop([0], axis=1, inplace=False)
 sixth = pd.read_csv('result/submitB_A6-0.02604.csv', header=None).reset_index().drop([0], axis=1, inplace=False)
+
+ensemble_result = 0.2 * first + 0.2 * second + 0.1 * third + 0.2 * fourth + 0.2 * fifth + 0.1 * sixth
+ensemble_result.to_csv('result/ensemble_submit.csv', header=None)
+
+sub_df = pd.read_csv('raw_data/answer_A.csv', header=None)
+sub_df['Value'] = ensemble_result[1]
+sub_df.to_csv('result/ensemble.csv', header=None, index=False)
+
 plt.plot(first['index'], first[1], 'r')
-plt.plot(second['index'], second[1], 'black')
+# plt.plot(second['index'], second[1], 'black')
 # plt.plot(third['index'], third[1], 'm')
-plt.plot(fifth['index'], fifth[1], 'b')
-plt.plot(sixth['index'], sixth[1], 'k')
-plt.plot(fourth['index'], fourth[1], 'g')
+# plt.plot(fifth['index'], fifth[1], 'b')
+# plt.plot(sixth['index'], sixth[1], 'k')
+# plt.plot(fourth['index'], fourth[1], 'g')
+plt.plot(ensemble_result['index'], ensemble_result[1], 'b')
 print(cal_MSE(second[1], fifth[1]))
 plt.legend(loc='upper left')
 plt.show()
